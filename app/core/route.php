2cf2af9cf16008f29231acf3bd8fcf21c3a2c496
Iskandarov Timur <timur.iskandarov96@gmail.com>
2022-03-11 13:02:04 +0600

    catch 404 controller

--- app/core/route.php
+++ app/core/route.php
@@ -76,7 +76,7 @@
             $host = 'https://'.$_SERVER['HTTP_HOST'].'/';
             header('HTTP/1.1 404 Not Found');
             header("Status: 404 Not Found");
-            header('Location:'.$host.'404');
+            header('Location:'.$host);
         }
     }
 ?>
