736d8f3649e1ac9b0ff173d7c38fa7f97b0bd2db
Iskandarov Timur <timur.iskandarov96@gmail.com>
2022-03-11 12:55:55 +0600

    push work

--- app/core/route.php
+++ app/core/route.php
@@ -73,10 +73,10 @@
         
         function ErrorPage404()
         {
-            // $host = 'https://'.$_SERVER['HTTP_HOST'].'/';
-            // header('HTTP/1.1 404 Not Found');
-            // header("Status: 404 Not Found");
-            // header('Location:'.$host.'404');
+            $host = 'https://'.$_SERVER['HTTP_HOST'].'/';
+            header('HTTP/1.1 404 Not Found');
+            header("Status: 404 Not Found");
+            header('Location:'.$host.'404');
         }
     }
 ?>
